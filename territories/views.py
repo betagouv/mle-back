7a4130b4765828f5a2e608e2c152b254080726ff
Marie-Laure Vernay <mlvernay@gmail.com>
2024-12-03 14:40:40 +0100

    Use a serializer for combined territories

--- territories/views.py
+++ territories/views.py
@@ -3,23 +3,16 @@ from rest_framework.views import APIView
 
 from territories.models import Academy, City, Department
 
-from .serializers import AcademySerializer, CitySerializer, DepartmentSerializer
+from .serializers import TerritoryCombinedSerializer
 
 
 class TerritoryCombinedListAPIView(APIView):
     def get(self, request, *args, **kwargs):
-        academies = Academy.objects.all()
-        departments = Department.objects.all()
-        cities = City.objects.all()
-
-        academy_serializer = AcademySerializer(academies, many=True)
-        department_serializer = DepartmentSerializer(departments, many=True)
-        city_serializer = CitySerializer(cities, many=True)
-
         data = {
-            "academies": academy_serializer.data,
-            "departments": department_serializer.data,
-            "cities": city_serializer.data,
+            "academies": Academy.objects.all(),
+            "departments": Department.objects.all(),
+            "cities": City.objects.all(),
         }
 
-        return Response(data)
+        serializer = TerritoryCombinedSerializer(data)
+        return Response(serializer.data)

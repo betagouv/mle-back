6f901d2dfa5ed71bb4f5074f1cc8eebf562a14e2
Marie-Laure Vernay <mlvernay@gmail.com>
2024-12-16 11:20:14 +0100

    Add a filter on nb_accessible_apartments

--- tests/accommodation/test_api.py
+++ tests/accommodation/test_api.py
@@ -12,16 +12,23 @@ class AccommodationListAPITests(APITestCase):
         self.accommodation_unpublished = AccommodationFactory(published=False)
         self.accommodation_no_geom = AccommodationFactory(geom=None)
 
+        self.accommodation_nantes_accessible = AccommodationFactory(
+            geom=Point(-1.5536, 47.2184), nb_accessible_apartments=2
+        )
+        self.accommodation_nantes_non_accessible = AccommodationFactory(
+            geom=Point(-1.5530, 47.2150), nb_accessible_apartments=0
+        )
+
     def test_accommodation_list_no_filter(self):
         response = self.client.get(reverse("accommodation-list"))
 
         assert response.status_code == 200
         results = response.json()
 
-        assert results["count"] == 2
+        assert results["count"] == 4
         assert results["page_size"] == 30
         assert results["next"] is None and results["previous"] is None
-        assert len(results["results"]["features"]) == 2
+        assert len(results["results"]["features"]) == 4
         returned_ids = [feature["id"] for feature in results["results"]["features"]]
         assert "slug" in results["results"]["features"][0]["properties"]
         assert self.accommodation_paris.id in returned_ids
@@ -29,16 +36,23 @@ class AccommodationListAPITests(APITestCase):
         assert self.accommodation_unpublished.id not in returned_ids
         assert self.accommodation_no_geom.id not in returned_ids
 
-    def test_accommodation_list_view_bbox_filter(self):
-        bbox = "2.30,48.80,2.40,48.90"
+    def test_accommodation_list_view_filters(self):
+        bbox = "-1.60,47.20,-1.50,47.30"  # Nantes
 
         response = self.client.get(reverse("accommodation-list"), {"bbox": bbox})
-
-        assert response.status_code == 200
         results = response.json()
 
-        assert results["count"] == 1
-        assert results["next"] is None and results["previous"] is None
+        assert len(results["results"]["features"]) == 2
+
+        returned_ids = [feature["id"] for feature in results["results"]["features"]]
+        assert self.accommodation_nantes_accessible.id in returned_ids
+        assert self.accommodation_nantes_non_accessible.id in returned_ids
+
+        response = self.client.get(reverse("accommodation-list"), {"bbox": bbox, "is_accessible": True})
+        results = response.json()
+
         assert len(results["results"]["features"]) == 1
-        feature = results["results"]["features"][0]
-        assert feature["id"] == self.accommodation_paris.id
+
+        returned_ids = [feature["id"] for feature in results["results"]["features"]]
+        assert self.accommodation_nantes_accessible.id in returned_ids
+        assert self.accommodation_nantes_non_accessible.id not in returned_ids

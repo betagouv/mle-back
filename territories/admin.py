2c35764f5d2fdc9b765b544a2f3ac02399fdc4e8
Marie-Laure Vernay <mlvernay@gmail.com>
2024-12-16 16:28:32 +0100

    Add Country
    Even if we will have only 1 country, the aim is to keep the same design for
    question answer: having all question answer attached to a territory.
    No need of boundary for country. Country not exposed in API.

--- territories/admin.py
+++ territories/admin.py
@@ -3,22 +3,28 @@ from django.contrib.gis.admin import OSMGeoAdmin
 
 from qa.admin import QuestionAnswerInline
 
-from .models import Academy, City, Department
+from .models import Academy, City, Country, Department
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    inlines = (QuestionAnswerInline,)
+    search_fields = ("name",)
 
 
 @admin.register(City)
 class CityAdmin(OSMGeoAdmin):
-    inlines = [QuestionAnswerInline]
-    search_fields = ["name", "postal_codes"]
+    inlines = (QuestionAnswerInline,)
+    search_fields = ("name", "postal_codes")
 
 
 @admin.register(Department)
 class DepartmentAdmin(OSMGeoAdmin):
-    inlines = [QuestionAnswerInline]
-    search_fields = ["name", "code"]
+    inlines = (QuestionAnswerInline,)
+    search_fields = ("name", "code")
 
 
 @admin.register(Academy)
 class AcademyAdmin(OSMGeoAdmin):
-    inlines = [QuestionAnswerInline]
-    search_fields = ["name"]
+    inlines = (QuestionAnswerInline,)
+    search_fields = ("name",)

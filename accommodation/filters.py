6fc7aca968ffeb95071ca1506b07d83e6fea65b1
Marie-Laure Vernay <mlvernay@gmail.com>
2024-12-23 09:53:25 +0100

    Filter accommodations around a given center point and a radius.

--- accommodation/filters.py
+++ accommodation/filters.py
@@ -1,4 +1,5 @@
-from django.contrib.gis.geos import Polygon
+from django.contrib.gis.geos import Point, Polygon
+from django.contrib.gis.measure import Distance
 from django_filters.rest_framework import FilterSet, filters
 from rest_framework.exceptions import ValidationError
 
@@ -8,6 +9,7 @@ from .models import Accommodation
 class AccommodationFilter(FilterSet):
     bbox = filters.CharFilter(method="filter_bbox", label="Bounding box")
     is_accessible = filters.BooleanFilter(method="filter_is_accessible", label="Only accessible accommodations")
+    center = filters.CharFilter(method="filter_center", label="Center point for radius filtering (lon,lat)")
 
     def filter_bbox(self, queryset, name, value):
         try:
@@ -24,6 +26,16 @@ class AccommodationFilter(FilterSet):
         if value is True:
             return queryset.filter(nb_accessible_apartments__gt=0)
         return queryset
+
+    def filter_center(self, queryset, name, value):
+        try:
+            lon, lat = map(float, value.split(","))
+            point = Point(lon, lat)
+            radius = float(self.data.get("radius") or 10)
+            distance = Distance(km=radius)
+            return queryset.filter(geom__distance_lte=(point, distance))
+        except (ValueError, TypeError):
+            raise ValidationError("Invalid center format. Should be 'longitude,latitude'. Radius must be a number.")
 
     class Meta:
         model = Accommodation

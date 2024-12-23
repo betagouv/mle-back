12b06845e8d7bd2bdea6fec39265085ed36feb7e
Marie-Laure Vernay <mlvernay@gmail.com>
2024-12-23 09:30:32 +0100

    JDE -> MLE

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -3,7 +3,7 @@ from .base import *  # noqa: F403
 DEBUG = False
 
 SITE_NAME = f"[STAGING] {SITE_NAME}"  # noqa: F405
-ALLOWED_HOSTS = ["jde-back-staging.osc-secnum-fr1.scalingo.io"]
+ALLOWED_HOSTS = ["mle-back-staging.osc-secnum-fr1.scalingo.io"]
 
 SECURE_HSTS_SECONDS = 3600
 SECURE_SSL_REDIRECT = True

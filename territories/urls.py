2645583300ee77d1ae75e24e0254cea84cee4033
Marie-Laure Vernay <mlvernay@gmail.com>
2024-12-24 14:15:37 +0100

    Add a dedicated enpoint to retrieve academies

--- territories/urls.py
+++ territories/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import TerritoryCombinedListAPIView
+from .views import AcademyListAPIView, TerritoryCombinedListAPIView
 
 urlpatterns = [
+    path("academies/", AcademyListAPIView.as_view(), name="academies-list"),
     path("", TerritoryCombinedListAPIView.as_view(), name="territory-combined-list"),
 ]

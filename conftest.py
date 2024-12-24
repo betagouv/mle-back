958393b7b4cd0a1e03885d7939690007322cb91a
Marie-Laure Vernay <mlvernay@gmail.com>
2024-12-24 11:56:45 +0100

    Fix tests.
    - Use an autouse to always mock the calls to geo.api.gouv.fr
    - create initial factories for CLEF import
    - remove the test on territory api with no filter as q is now mandatory

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import os
+from unittest import mock
+from urllib.parse import parse_qs, urlparse
 
 import pytest
+import requests
 from django.db import connection
 
 
@@ -17,3 +20,31 @@ def django_db_setup(django_db_setup, django_db_blocker):
         cursor.execute(
             "ALTER TEXT SEARCH CONFIGURATION french_unaccent ALTER MAPPING FOR hword, hword_part, word WITH unaccent, french_stem"
         )
+
+
+def mock_requests_get(url, *args, **kwargs):
+    parsed_url = urlparse(url)
+    params = parse_qs(parsed_url.query)
+
+    if parsed_url.netloc == "geo.api.gouv.fr":
+        postal_code = params.get("codePostal", [None])[0]
+        city = params.get("nom", [None])[0]
+
+        mock_response = mock.Mock()
+        mock_response.json.return_value = [
+            {
+                "nom": city,
+                "codesPostaux": [postal_code],
+                "codeDepartement": f"{postal_code[0:2]}",
+                "contour": "POLYGON((2.3522 48.8566, 2.3523 48.8567, 2.3524 48.8568, 2.3525 48.8567, 2.3522 48.8566))",
+            }
+        ]
+        return mock_response
+
+    return requests.get(url, *args, **kwargs)
+
+
+@pytest.fixture(autouse=True)
+def mock_get_requests():
+    with mock.patch("requests.get", side_effect=mock_requests_get):
+        yield

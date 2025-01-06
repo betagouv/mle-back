e51421ce01babe2a43ec08f1f57abf50c77cd991
Marie-Laure Vernay <mlvernay@gmail.com>
2025-01-06 15:32:59 +0100

    Api for deparments list.
    Also rename City view for consistency.

--- territories/urls.py
+++ territories/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AcademyListAPIView, CitiesListApiView, TerritoryCombinedListAPIView
+from .views import AcademyListAPIView, CityListAPIView, DepartmentListAPIView, TerritoryCombinedListAPIView
 
 urlpatterns = [
     path("academies/", AcademyListAPIView.as_view(), name="academies-list"),
-    path("cities/", CitiesListApiView.as_view(), name="cities-list"),
+    path("cities/", CityListAPIView.as_view(), name="cities-list"),
+    path("departments/", DepartmentListAPIView.as_view(), name="departments-list"),
     path("", TerritoryCombinedListAPIView.as_view(), name="territory-combined-list"),
 ]

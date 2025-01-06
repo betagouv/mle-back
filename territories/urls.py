d0a05a441913cde8b72be36a687a433be88e704a
Marie-Laure Vernay <mlvernay@gmail.com>
2025-01-06 09:25:48 +0100

    Retrieve a list of cities from a given department code.

--- territories/urls.py
+++ territories/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import AcademyListAPIView, TerritoryCombinedListAPIView
+from .views import AcademyListAPIView, CitiesListApiView, TerritoryCombinedListAPIView
 
 urlpatterns = [
     path("academies/", AcademyListAPIView.as_view(), name="academies-list"),
+    path("cities/", CitiesListApiView.as_view(), name="cities-list"),
     path("", TerritoryCombinedListAPIView.as_view(), name="territory-combined-list"),
 ]

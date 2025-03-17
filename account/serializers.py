3cf5a08ee7b6d5b6dd09f07e88c3886323c66a68
Marie-Laure Vernay <mlvernay@gmail.com>
2025-03-17 14:44:14 +0100

    Expose owner in API

---
+++ account/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from common.serializers import Base64BinaryField
+
+from .models import Owner
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    image_base64 = Base64BinaryField(required=False, allow_null=True)
+
+    class Meta:
+        model = Owner
+        fields = ("name", "slug", "url", "image_base64")

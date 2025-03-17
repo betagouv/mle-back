7915d7d27710ba3423b3ecf34109585eeb437635
Marie-Laure Vernay <mlvernay@gmail.com>
2025-03-17 10:45:11 +0100

    Owner admin and API restitution

---
+++ account/admin.py
@@ -0,0 +1,24 @@
+import base64
+
+from django.contrib import admin
+from django.utils.html import format_html
+
+from .forms import OwnerAdminForm
+from .models import Owner
+
+
+class OwnerAdmin(admin.ModelAdmin):
+    form = OwnerAdminForm
+    list_display = ("name", "image_preview")
+    readonly_fields = ("image_preview",)
+
+    def image_preview(self, obj):
+        if obj.image:
+            image_base64 = base64.b64encode(obj.image).decode("utf-8")
+            return format_html(f'<img src="data:image/png;base64,{image_base64}" width="100" height="100"/>')
+        return "No Image"
+
+    image_preview.short_description = "Image"
+
+
+admin.site.register(Owner, OwnerAdmin)

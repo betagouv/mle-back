a4c03170048340fe58007fa7dda2c2c62122f083
Marie-Laure Vernay <mlvernay@gmail.com>
2025-04-08 10:33:51 +0200

    Add a "view on API" link in city admin detail page.

--- territories/admin.py
+++ territories/admin.py
@@ -19,6 +19,18 @@ class CityAdmin(OSMGeoAdmin):
     search_fields = ("name", "postal_codes", "insee_codes")
     list_filter = ("popular",)
 
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        if not extra_context:
+            extra_context = {}
+
+        obj = self.get_object(request, object_id)
+        if obj:
+            extra_context["api_url"] = obj.get_absolute_detail_api_url()
+
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
+
+    change_form_template = "admin/change_form.html"
+
 
 @admin.register(Department)
 class DepartmentAdmin(OSMGeoAdmin):

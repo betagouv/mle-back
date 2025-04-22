9848da2fed47111533a0fe8941bb547d087b0fd0
Marie-Laure Vernay <mlvernay@gmail.com>
2025-04-22 14:58:30 +0200

    Add allowed hosts

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -4,6 +4,11 @@ DEBUG = False
 
 SITE_NAME = f"[STAGING] {SITE_NAME}"  # noqa: F405
 ALLOWED_HOSTS = ["mle-back-staging.osc-secnum-fr1.scalingo.io"]
+ALLOWED_HOSTS = [
+    "mle-back-staging.osc-secnum-fr1.scalingo.io",
+    "test.monlogementetudiant.beta.gouv.fr",
+    "localhost:3000",
+]
 
 SECURE_HSTS_SECONDS = 3600
 SECURE_SSL_REDIRECT = True

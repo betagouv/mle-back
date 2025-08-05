8f1f299482ff4cf98526e28dedb0af50a21a5653
Marie-Laure Vernay <mlvernay@gmail.com>
2025-08-05 14:18:43 +0200

    Create institution filter and factorize

--- accommodation/filters.py
+++ accommodation/filters.py
@@ -1,30 +1,15 @@
-from django.contrib.gis.geos import Point, Polygon
-from django.contrib.gis.measure import Distance
-from django_filters.rest_framework import FilterSet, filters
-from rest_framework.exceptions import ValidationError
+from django_filters.rest_framework import filters
 
-from .models import Accommodation
+from accommodation.models import Accommodation
+from common.filters import BaseFilter
 
 
-class AccommodationFilter(FilterSet):
-    bbox = filters.CharFilter(method="filter_bbox", label="Bounding box")
+class AccommodationFilter(BaseFilter):
     is_accessible = filters.BooleanFilter(method="filter_is_accessible", label="Only accessible accommodations")
     has_coliving = filters.BooleanFilter(
         method="filter_has_coliving", label="Only accommodations with coliving apartments"
     )
-    center = filters.CharFilter(method="filter_center", label="Center point for radius filtering (lon,lat)")
     price_max = filters.NumberFilter(method="filter_price_max", label="Price max in euros")
-
-    def filter_bbox(self, queryset, name, value):
-        try:
-            bbox = list(map(float, value.split(",")))
-            if len(bbox) != 4:
-                raise ValidationError("Invalid bbox format. Should be 'xmin,ymin,xmax,ymax'.")
-
-            polygon = Polygon.from_bbox(bbox)
-            return queryset.filter(geom__within=polygon)
-        except ValueError:
-            raise ValidationError("Invalid bbox format. Coordinates should be numbers.")
 
     def filter_is_accessible(self, queryset, name, value):
         if value is True:
@@ -35,16 +20,6 @@ class AccommodationFilter(FilterSet):
         if value is True:
             return queryset.filter(nb_coliving_apartments__gt=0)
         return queryset
-
-    def filter_center(self, queryset, name, value):
-        try:
-            lon, lat = map(float, value.split(","))
-            point = Point(lon, lat)
-            radius = float(self.data.get("radius") or 10)
-            distance = Distance(km=radius)
-            return queryset.filter(geom__distance_lte=(point, distance))
-        except (ValueError, TypeError):
-            raise ValidationError("Invalid center format. Should be 'longitude,latitude'. Radius must be a number.")
 
     def filter_price_max(self, queryset, name, value):
         if value is None:

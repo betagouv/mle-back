fe1b9bfc769d603a0a16524883dc91457dc0eae0
Marie-Laure Vernay <mlvernay@gmail.com>
2025-10-14 15:26:09 +0200

    Setup sentry

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,3 +1,5 @@
+import sentry_sdk
+
 from .base import *  # noqa: F403
 
 DEBUG = True
@@ -11,3 +13,14 @@ CSRF_COOKIE_SECURE = False
 FRONT_SITE_URL = "http://localhost:8000"
 
 AWS_SUFFIX_DIR = "-dev"
+
+
+if SENTRY_DSN is not None:  # noqa: F405
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,  # noqa: F405
+        integrations=[DjangoIntegration()],
+        send_default_pii=True,
+        environment="dev",
+    )

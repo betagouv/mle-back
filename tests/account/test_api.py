840a4444baf92583fcf181383be314ce3a6f8e5c
Marie-Laure Vernay <mlvernay@gmail.com>
2025-10-21 15:11:09 +0200

    API route to request magic link

--- tests/account/test_api.py
+++ tests/account/test_api.py
@@ -1,8 +1,10 @@
+from unittest.mock import patch
+
 import pytest
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
-from .factories import OwnerFactory
+from .factories import OwnerFactory, UserFactory
 
 
 class AccountAPITests(APITestCase):
@@ -12,3 +14,21 @@ class AccountAPITests(APITestCase):
         response = self.client.get(reverse("owner-list"))
         assert response.status_code == 200
         assert len(response.json()) == 5
+
+    @patch("sib_api_v3_sdk.TransactionalEmailsApi.send_transac_email")
+    def test_request_magic_link(self, mock_send_email):
+        mock_send_email.return_value = None
+
+        response = self.client.post(reverse("request-magic-link"), {"email": "test@test.com"})
+        assert response.status_code == 200
+        assert response.json() == {
+            "detail": "If an account exists with this email, you will receive a link to log in. Please contact xxx in case of problem."
+        }
+
+        UserFactory.create(email="test@test.com")
+        response = self.client.post(reverse("request-magic-link"), {"email": "test@test.com"})
+        assert response.status_code == 200
+        assert response.json() == {
+            "detail": "If an account exists with this email, you will receive a link to log in. Please contact xxx in case of problem."
+        }
+        assert mock_send_email.call_count == 1

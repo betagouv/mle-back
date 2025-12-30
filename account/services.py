73a7e10ba73bff615cf92bb5d3ec1297071bf53e
Kevin GALLET <kevingallet@gmail.com>
2025-12-30 13:57:29 +0100

    fix: resest password uri

--- account/services.py
+++ account/services.py
@@ -54,4 +54,4 @@ def build_password_reset_link(user, frontend_base_url: str) -> str:
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     token = PasswordResetTokenGenerator().make_token(user)
 
-    return f"{frontend_base_url}/mot-de-passe-oublie?uid={uid}&token={token}"
+    return f"{frontend_base_url}/reinitialiser-son-mot-de-passe?uid={uid}&token={token}"

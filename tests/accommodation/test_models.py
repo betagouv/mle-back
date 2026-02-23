961eacd3ab84d15e6c814011b8753582498ee432
pierre chéné <pierre.chene44@gmail.com>
2026-02-23 10:27:25 +0100

    Add external_reference to match external api easily

--- tests/accommodation/test_models.py
+++ tests/accommodation/test_models.py
@@ -3,6 +3,7 @@ from django.contrib.gis.geos import Point
 from django.core.exceptions import ValidationError
 
 from accommodation.models import Accommodation
+from tests.account.factories import OwnerFactory
 
 
 @pytest.mark.django_db
@@ -34,3 +35,38 @@ class TestAccommodation:
         with pytest.raises(ValidationError) as e:
             acc.full_clean()
         assert "nb_t1_available" in e.value.message_dict
+
+    def test_raises_error_if_owner_and_external_reference_are_not_unique(self):
+        owner = OwnerFactory()
+
+        Accommodation.objects.create(
+            owner=owner,
+            external_reference="1234567890",
+            name="Résidence A",
+            address="1 rue test",
+            city="Lyon",
+            postal_code="69000",
+            geom=Point(4.85, 45.75),
+            nb_t1=5,
+            nb_t1_available=5,
+        )
+
+        acc = Accommodation(
+            owner=owner,
+            external_reference="1234567890",
+            name="Résidence B",
+            address="2 rue test",
+            city="Lyon",
+            postal_code="69000",
+            geom=Point(4.85, 45.75),
+            nb_t1=5,
+            nb_t1_available=5,
+        )
+
+        with pytest.raises(ValidationError) as e:
+            acc.full_clean()
+
+        assert any(
+            "Un objet Résidence avec ces champs Owner et External reference existe déjà." in msg
+            for msg in e.value.messages
+        )

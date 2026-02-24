7cbc605afe0c79e674d003c035765896d07c0aa6
pierre chéné <pierre.chene44@gmail.com>
2026-02-24 09:03:45 +0100

    WIP

---
+++ accommodation/management/commands/import_oh_mon_appart_etudiant.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Import OH Mon Appart Etudiant data"
+
+    def handle(self, *args, **options):
+        pass
